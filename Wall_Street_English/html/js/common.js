22fc8956c99bd4dfedacf89e3e0a4e712ea92c1c
alianggitok <alianggmok@gmail.com>
2014-06-29 04:11:58 +0800

    leveltest

--- Wall_Street_English/html/js/common.js
+++ Wall_Street_English/html/js/common.js
@@ -3,6 +3,7 @@
 	var _window=$(window);
 
 	return ui={
+		//navig initialize
 		navigInit:function(triggerobj,activeObj){
 			var _triggerobj=$(triggerobj), _activeObj=$(activeObj);
 			var _bodyChild=$('body').children(':not("header,footer,script,link")');
@@ -14,7 +15,7 @@
 			if(height>minHeight){
 				_activeObj.height(height);
 			};
-			_triggerobj.on('click',function(e){
+			_triggerobj.off('click.navig').on('click.navig',function(e){
 				if(_activeObj.is(':visible')){
 					_activeObj.slideUp();
 				}else{
@@ -22,7 +23,34 @@
 				};
 				e.preventDefault();
 			});
+			$(document).off('click.navig').on('click.navig', function (e) {
+				var _objs=$(triggerobj+','+activeObj+' li'+', .layout-navig .nav');
+				if (($(e.target).closest(_objs).is(_objs) || $(e.target).is(_objs)) == false) {
+					_activeObj.slideUp();
+				};
+			});
+		},
+
+		checkboxInit:function(obj){
+			var _obj=$(obj);
+			var stateChecked='checkbox-checked';
+			_obj.off('click.checkbox').on('click.checkbox',function(){
+				if($(this).is('.'+stateChecked)){
+					$(this).removeClass(stateChecked);
+				}else{
+					$(this).addClass(stateChecked);
+				};
+			});
+			_obj.parent('label').off('click.checkbox').on('click.checkbox',function(){
+				if($(this).children(obj).is('.'+stateChecked)){
+					$(this).children(obj).removeClass(stateChecked);
+				}else{
+					$(this).children(obj).addClass(stateChecked);
+				};
+			});
 		}
+
+
 	};
 
 }());
@@ -30,5 +58,25 @@
 
 /*********** exec ***********/
 $(function(){
-	ui.navigInit('.layout-header .navig-trigger','.layout-navig .navig')
+	var navigTriggerObj='.layout-header .navig-trigger .ico';
+
+	ui.navigInit(navigTriggerObj,'.layout-navig .navig');
+	ui.checkboxInit('.checkbox');
+
+	//back to navigation
+	$('.layout-navig .nav .nav-backnavig').on('click',function(){
+		$(navigTriggerObj).click();
+	});
+
+	//list summary fold
+	$('.mainlist .summary .trigger').on('click',function(e){
+		var _activeObj=$(this).next('');
+		if(_activeObj.is(':visible')){
+			_activeObj.slideUp();
+		}else{
+			_activeObj.slideDown();
+		};
+		e.preventDefault();
+	});
+
 });
