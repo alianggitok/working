211494092c659e5a788b74ba11958996dbf936dc
alianggitok <alianggmok@gmail.com>
2014-07-06 22:46:53 +0800

    audio.js

--- Wall_Street_English/html/js/audio.js
+++ Wall_Street_English/html/js/audio.js
@@ -1,27 +1,52 @@
-audiojs.events.ready(function () {
- 	var as = audiojs.createAll();
-});
+function audioInit() {
+	audiojs.events.ready(function () {
+		var as = audiojs.createAll();
+	});
+};
 
 $(function () {
 	var listenbar = $('.listenbar'),
 		listenbtn = null,
 		listenAudio = null,
 		checkStatusTimer = null,
+		checkStatusDelay = null,
 		isPlaying = null;
 
 	listenbar.each(function () {
 		listenbar = $(this);
 		listenbtn = listenbar.find('.btn-listen');
+		listenAudio = listenbar.find('audio');
 
 		listenbtn.click(function () {
+			clearInterval(checkStatusTimer);
+			checkStatusTimer = null;
+			clearTimeout(checkStatusDelay);
+			checkStatusDelay = null;
+
+			$('.audiojs').remove();
+			listenAudio.appendTo(listenbar);
+			audioInit();
+			listenbar.removeClass('listenbar-playing');
 			listenbar.find('.play-pause').click();
-			isPlaying = listenbar.find('.playing').is('.playing');
-			console.log(isPlaying);
-			if (isPlaying) {
-				listenbar.addClass('listenbar-playing');
-			} else {
-				listenbar.removeClass('listenbar-playing');
-			};
+
+			checkStatusDelay = setTimeout(function () {
+				isPlaying = listenbar.find('.playing').is('.playing');
+				if (isPlaying) {
+					listenbar.addClass('listenbar-playing');
+					clearTimeout(checkStatusDelay);
+					checkStatusDelay = null;
+				} else {
+					listenbar.removeClass('listenbar-playing');
+				};
+			},300);
+			checkStatusTimer = setInterval(function () {
+				isPlaying = listenbar.find('.playing').is('.playing');
+				if (!isPlaying) {
+					listenbar.removeClass('listenbar-playing');
+					clearInterval(checkStatusTimer);
+					checkStatusTimer = null;
+				};
+			}, 500);
 		});
 	});
 });
