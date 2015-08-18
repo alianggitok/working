f8cf51229fb774b8070ab7a84aa8f0793c1c3a61
Warren Chen <alianggmok@gmail.com>
2015-08-18 16:26:21 +0800

    tidy folder app.ui.message.css({autoHide}) api doc

---
+++ 旅客系统/sapis/doc/api.md
@@ -0,0 +1,203 @@
+#SAPIS Javascript Library API
+
+*—— Warren* in *2015-8-18*
+
+基于：
+
+- jQuery
+- [Semantic UI](http://semantic-ui.com/)
+- [jquery.datatimepicker](http://xdsoft.net/jqplugins/datetimepicker/)
+
+&nbsp;
+##sys
+###app.checkRE()
+- **说明：** 检测浏览器，默认检测是否为 chrome，且版本不低于40+。
+- **参数：** 无
+- **示例：**
+
+		app.checkRE();
+
+&nbsp;
+##cookie
+###app.cookie.set()
+- **说明：** 以键值对形式设置一个或多个 cookie 项。
+- **参数：** param（object，键值对）
+	- key:value
+- **示例：**
+
+		app.cookie.set({
+			key:value,
+			...
+		});
+
+###app.cookie.removeAll()
+- **说明：** 清除所有本 app 相关的 cookie。
+- **参数：** 无
+- **示例：**
+
+		app.cookie.removeAll();
+
+&nbsp;
+##ajax
+###app.ajax()
+- **说明：** 对 jQuery 的 ajax 方法的封装。
+- **参数：** opts（object，配置参数），callback（function）
+	- **opts**：配置参数
+		- url：ajax请求地址，必选
+		- data：请求参数，object
+		- dataType：请求类型，默认 'json'
+		- type：默认POST
+		- bufferContext：缓冲UI的上下文选择器，默认 'body'
+		- bufferMsg：缓冲UI中的提示文本，默认 '处理中'
+	- **callback**：请求成功后的回调
+- **示例：**
+
+		app.ajax({
+			url:'/action/do',
+			data:{
+				id:11,
+				action:'delete'
+			}
+		});
+
+&nbsp;
+##初始化
+###app.ui.init()
+- **说明：** 初始化应用，通常将前端框架（如 Semantic-UI）的组件初始化方法放在这里。慎用，除非页面或模块重载，否则避免重复调用。
+- **参数：** 无
+- **示例：**
+
+		$(function(){
+			app.ui.init();
+		});
+
+&nbsp;
+##app.ui.buffer
+执行缓冲 UI，也就是Loading，对 ajax 等需要用户长时间等待响应的操作给予反馈。
+###app.ui.buffer.show()
+- **说明：** 显示 buffer，并返回该 buffer DOM对象。
+- **参数：** opts（object，配置参数，可选）
+	- **opts**
+		- context：上下文选择器，用来确定 buffer 覆盖范围，默认 'body'
+		- msg：提示文本的内容，默认 '处理中'
+		- size：尺寸，默认值 'medium'，可选值有：mini、small、medium、large
+		- opacity：遮罩层透明度设置，默认值0.7
+- **示例：**
+
+		var buffer=app.ui.buffer.show({
+			context:'.container',
+			msg:'读取中'
+		});
+
+###app.ui.buffer.hide()
+- **说明：** 隐藏 buffer 对象。
+- **参数：** obj（由app.ui.buffer.show() 返回的 buffer 对象）
+- **示例：**
+
+		var buffer=app.ui.buffer.show();
+		app.ui.buffer.hide(buffer);
+
+&nbsp;
+##导航
+###app.ui.navigBuild()
+- **说明：** 生成导航（左侧一二级 menu，右侧三级 tabs），通常在页面载入时调用一次。慎用，无需重复调用。
+- **参数：** data（菜单数据，数组形式，详细见 config.js中的模拟菜单数据）
+- **示例：**
+
+		app.ui.navigBuild(data);
+
+&nbsp;
+##模块加载
+###app.ui.loadModule()
+- **说明：** 读取模块内容。
+- **参数：** opts（object，配置参数）
+	- **opts** 
+		- obj：指定内容的载体选择器，必选
+		- href：请求内容的地址，必选
+		- bufferContext：缓冲UI的上下文选择器，默认 'body'，慎用
+		- scrollToTop：载入前是否将载体滚动到顶部，默认 true
+		- success：成功后的回调，function
+- **示例：**
+
+		app.ui.loadModule({
+			obj:'.moduleWrapper',
+			href:'/module/demo.html',
+			success:function(){
+				alert('success!');
+			}
+		});
+
+&nbsp;
+##模态弹窗
+###app.ui.confirm()
+- **说明：** 确认弹窗。
+- **参数：** opts（object，配置参数）
+	- **opts** 
+		- title：弹窗标题，默认 '确认'
+		- msg：提示文本
+		- icon：图标名称，默认 'help'
+		- buttons：array，按钮的 html，将会被插入到 actions 层
+		- onApprove：function，点击 approve（同意、是） 类型按钮后的回调
+		- onDeny：function，点击 deny（拒绝、否） 类型按钮后的回调
+- **示例：**
+
+		app.ui.confirm({
+			title:'是否删除',
+			msg:'您是否要删除本条数据？',
+			onApprove:function(){
+				delete();
+			}
+		});
+
+###app.ui.alert()
+- **说明：** 提示弹窗。
+- **参数：** opts（object），说明参照 app.ui.confirm()
+- **示例：**
+
+		app.ui.alert({
+			icon:'warning',
+			msg:'您有三条未读的短消息！'
+		});
+
+&nbsp;
+##对话框
+###app.ui.dialog()
+- **说明：** 初始化对话框，并显示，返回该对话框 DOM对象。对话框内容包括标题、按钮事先都在 html 中书写好，html 结构参考相关页面。
+- **参数：** obj（对象选择器，建议用id），opts（object，配置参数）
+	- **opts** 
+		- style：
+			- basic：窗体样式，默认 'small'，可选参数：'small'、'large'、'fullscreen'
+		- closeBtn：是否显示右上角关闭按钮，默认 true
+		- closeable：点击遮罩层是否关闭，默认 false
+		- blurring：遮罩层是否高斯模糊，默认false
+		- dimmersettings：
+			- opacity：遮罩层透明度，默认0.6
+- **示例：**
+
+		app.ui.dialog('#dialog',{
+			style:{
+				basic:'fullscreen'
+			}
+		});
+
+&nbsp;
+##消息
+###app.ui.message()
+- **说明：** 显示一个消息层。
+- **参数：** 
+	- **opts** 
+		- type：消息类型，默认 'info'，可选值有：'success'、'info'、'warning'、'error'
+		- size：尺寸，默认 'small'
+		- msg：消息内容文本
+		- closeBtn：是否显示关闭按钮，默认 true
+		- icon：图标名称，默认 'smile'
+		- context：消息层出现位置的上下文
+		- autoHide：自动关闭时间，如4000，表示4秒后关闭，默认 false
+- **示例：**
+
+		app.ui.message({
+			msg:'成功啦！',
+			closeBtn:false,
+			autoHide:3000
+		});
+
---
+++ 旅客系统/日志系统/js/common.js
@@ -0,0 +1,570 @@
+;(function($,app,window){
+
+	app.elems={
+//			leftSide:'#leftSide',
+//			top:'#top',
+//			leftSideTrigger:'#leftSideTrigger',
+			btnLogout:'#btn-logout',
+			pageBuffer:'#pageBuffer',
+//			module:'#module',
+//			alarm:'#alarm',
+//			main:'#main',
+//			header:'#header',
+//			topNavig:'#topNavig',
+//			contents:'#contents',
+//			searchLabel:'#labels-search',
+			searchForm:'#form-search',
+			chartBlocks:'#chart-blocks .ui-chart-block'
+		 };
+	
+	var elems=app.elems,
+		 animateDuration=app.settings.animateDuration;
+	
+	app.checkRE=function(){
+		var userAgent=window.navigator.userAgent,
+			 regexp=/chrome\/[\d\.]+/ig,
+			 isMatch=regexp.test(userAgent),
+			 matchedInfo=null,
+			 version=null;
+
+		console.warn('check client:',isMatch);
+		if(isMatch){
+			matchedInfo=userAgent.match(regexp)[0];
+			version=matchedInfo.match(/\d+/ig)[0];
+			if(version<40){
+//				app.ui.alert({
+//					title:'运行环境检测',
+//					msg:'您当前使用的版本号较低，请更换较新版本的浏览器！'
+//				});
+				alert('当前浏览器版本较低，请更换较新的版本，推荐版本号：40+。');
+			}
+		}else{
+//			app.ui.alert({
+//				title:'运行环境检测',
+//				msg:'您当前使用的浏览器不是 Chrome，将无法正常使用，请更换浏览器！'
+//			});
+			alert(app.info.name+' 无法在当前浏览器上正常运行，请更换为 Chrome 浏览器！');
+		}
+
+	};
+
+	app.cookie={
+		setting:{
+			path:app.path.root
+		},
+		set:function(param){
+			//usage: app.cookie.set({key:value});
+			var key=null;
+			console.log('set cookies');
+			for(key in param){
+				$.cookie(key, param[key], app.cookie.setting.path);
+			}
+		},
+		removeAll:function(){
+			//usage: app.cookie.removeAll();
+			var cookies=$.cookie(),
+				 key=null;
+			console.log('remove all cookies');
+			for(key in cookies){
+				$.removeCookie(key, app.cookie.setting.path);
+			}
+		}
+	};
+	
+	app.ajax=function(opts,callback){//ajax数据交互方法
+		var buffer=null;
+		var dfts={
+			url:null,
+			data:null,
+			global:false,
+			async: true,
+			cache:false,
+			dataType:'json',
+			type: 'POST',
+			timeout:app.settings.resourceTimeout,
+			buffer:true,
+			bufferContext:'body',
+			bufferMsg:'处理中',
+			beforeSend:function(){
+				if(opts.buffer){
+					buffer=app.ui.buffer.show({
+						msg:opts.bufferMsg,
+						context:opts.bufferContext
+					});
+				}
+			},
+			error:function(XMLHttpRequest, textStatus, errorThrown){
+				var msg='Ajax过程错误，或联系管理员：\n'+
+						 'XMLHttpRequest: '+XMLHttpRequest+'\n'+
+						 'textStatus: '+textStatus+'\n'+
+						 'errorThrown: '+errorThrown;
+				alert(msg);
+				console.log(XMLHttpRequest,textStatus,errorThrown);
+				if(opts.buffer){
+					app.ui.buffer.hide(buffer);
+				}
+			},
+			success:function(result){
+				callback(result);
+			},
+			complete:function(){
+				if(opts.buffer){
+					app.ui.buffer.hide(buffer);
+				}
+			}
+		};
+
+		opts=$.extend(dfts,opts);
+
+		callback=((callback||typeof(callback)!=='undefined')?callback:function(){});
+
+		if(!opts.url){
+			alert('app.ajax()，请配置url');
+			return;
+		}
+//		if(!opts.data){
+//			alert('ajax.exec()，请配置data');
+//			return;
+//		}
+
+		$.ajax(opts);
+
+	};
+
+	app.ui={
+		init:function(){
+			
+			console.log('initialize');
+			
+			//components
+			$('.ui.accordion').accordion();
+			$('.ui.dropdown').dropdown();
+			$('.ui-popup').popup({
+				inline: false,
+				exclusive:true,
+				className:{
+					popup:'ui inverted mini popup'
+				}
+			});
+			$('.ui.checkbox').checkbox();
+			
+			$('.ui-datapicker').datetimepicker({
+				lang:'zh',
+				timepicker:false,
+				format:'Y/m/d',
+				closeOnDateSelect:true,
+				closeOnWithoutClick:true,
+				defaultSelect:false,
+				mask:false,
+				scrollInput:false,
+				scrollMonth:false
+			});
+			
+		},
+		response:function(){
+			console.warn('response');
+			var width=$(window).width();
+			if(width>960){
+				console.log('response: medium');
+//				$(elems.leftSide).addClass('large').removeClass('small labeled icon');
+				$('.ui.table').removeClass('very compact small');
+//				$(elems.top).find('.header').html(app.info.fullTitle);
+			}else if(width<=960&&width>680){
+				console.log('response: small');
+//				$(elems.leftSide).addClass('small').removeClass('large labeled icon');
+				$('.ui.table').addClass('small').removeClass('very compact');
+//				$(elems.top).find('.header').html(app.info.title);
+			}else if(width<=680){
+				console.log('response: tiny');
+//				$(elems.leftSide).addClass('labeled icon').removeClass('small large');
+				$('.ui.table').addClass('very compact');
+//				$(elems.top).find('.header').html(app.info.sortTitle);
+			}
+//			app.ui.draw();
+		},
+		draw:function(leftSideVisibled){
+			console.warn('draw');
+		},
+		buffer:{
+			show:function(opts){
+				var dfts={
+						html:''+
+						 '<div class="ui dimmer">'+
+						 '	<div class="ui text loader"></div>'+
+						 '</div>',
+						context:'body',
+						msg:'处理中',
+						size:'medium',//mini,small,medium,large
+						inverted:false,//反色
+						closable:false,//点击留白关闭
+						duration:animateDuration,//动画持续时间
+						opacity:0.7//透明
+					};
+					opts=$.extend(dfts,opts);
+				
+				var obj=$(opts.html),
+					 loader=obj.find('.ui.loader');
+
+				if(opts.inverted){
+					obj.addClass('inverted');
+				}
+				loader.addClass(opts.size).html(opts.msg);
+
+				$(opts.context).append(obj);
+				
+				obj.dimmer({
+					closable:opts.closable,
+					duration:{
+						show:opts.duration,
+						hide:opts.duration
+					},
+					opacity:opts.opacity
+				});
+				obj.dimmer('show');
+				
+				console.log(opts.msg);
+				return obj;
+			},
+			hide:function(obj){
+				$(obj).dimmer('hide');
+				var duration=$(obj).dimmer('get duration');
+				window.setTimeout(function(){
+					obj.remove();
+					app.ui.draw();
+				},duration);
+			}
+		},
+		loadModule:function(opts){
+			var dfts={
+				obj:elems.module,
+				href:'/',
+				bufferContext:'body',
+				scrollToTop:true,
+				success:function(){}
+			};
+			opts=$.extend(dfts,opts);
+			
+			if(opts.scrollToTop){
+				$(window).scrollTop(0);
+			}
+			
+			$(opts.obj).empty();
+			//destory dimmers
+			$('.ui.dimmer.modals').remove();
+			//destory datetimepicker
+			$('.ui-datapicker').datetimepicker('destroy');
+			$(window).off('resize.xdsoft');
+			$('.xdsoft_datetimepicker').remove();
+
+			app.ajax({
+				dataType:'html',
+				type:'GET',
+				bufferContext:opts.bufferContext,
+				url:opts.href
+			},function(html){
+				console.warn('loading href:',opts.href);
+				$(opts.obj).html(html);
+				opts.success();
+				app.ui.init();
+			});
+		},
+		modal:function(opts){
+			var obj=null;
+			var dfts={
+				icon:'help',//图标，基于semantic ui
+				title:'title',//标题
+				msg:'msg',//提示内容
+				style:{//样式，基于semantic ui
+					basic:'',//basic, size
+					header:'left aligned',
+					content:'left aligned',
+					actions:'right aligned',
+					buttons:''//inverted
+				},
+				buttons:[],//按钮，基于semantic ui
+				allowMultiple:false,//是否同时打开多个
+				context:'body',//上下文
+				closeBtn:false,//是否显示关闭按钮
+				closable:false,//点击遮罩区关闭
+				transition:'scale',//呈现动画
+				blurring:true,//遮罩模糊
+				duration:animateDuration,
+				selector:{
+					approve:'.actions .approve',
+					deny:'.actions .deny',
+					close:'.actions .close,.ui-dialog-close'
+				},
+				dimmerSettings:{
+					opacity:0.7,
+					onChange:function(){
+						app.ui.draw();
+					}
+				},
+				onHidden:function(){
+					if(!opts.allowMultiple){
+						obj.remove();
+					}
+				},
+				html:''+
+					'<div class="ui coupled modal">'+
+					'	<i class="close icon ui-dialog-close"></i>'+
+					'	<div class="header"></div>'+
+					'	<div class="content"></div>'+
+					'	<div class="actions"></div>'+
+					'</div>'
+			};
+			opts=$.extend(dfts,opts);
+
+			obj=$(opts.html);
+			
+			if(opts.icon){
+				obj.children('.header').prepend('<i class="'+opts.icon+' icon"></i>');
+			}
+			
+			if(!opts.blurring){
+				$(opts.context).removeClass('blurring');
+			}
+			
+			if(!opts.closeBtn){
+				obj.find('.ui-dialog-close').hide();
+			}
+
+			obj.addClass(opts.style.basic);
+			obj.children('.header').first().addClass(opts.style.header).append(opts.title);
+			obj.children('.content').first().addClass(opts.style.content).append(opts.msg);
+			obj.children('.actions').first().addClass(opts.style.actions);
+			for(var i=0,len=opts.buttons.length; i<len; i+=1){
+				obj.find('.actions').append($(opts.buttons[i]).addClass(opts.style.buttons));
+			}
+
+			obj.modal(opts);
+
+			return obj;
+		},
+		confirm:function(opts){
+			var obj=null;
+			var dfts={
+				title:'确认',
+				msg:'是否？',
+				icon:'help',
+				allowMultiple:false,
+				style:{
+					basic:'small',//basic,size
+					header:'left aligned',
+					content:'center aligned',
+					actions:'right aligned',
+					buttons:''//inverted
+				},
+				buttons:[//按钮，基于semantic ui
+					'<button class="ui green button approve"><i class="checkmark icon"></i>同意</button>',
+					'<button class="ui red button deny"><i class="remove icon"></i>拒绝</button>'
+				],
+				onApprove:function(){
+				},
+				onDeny:function(){
+				}
+			};
+			opts=$.extend(dfts,opts);
+			
+			obj=app.ui.modal(opts);
+			obj.modal('show');
+		},
+		alert:function(opts){
+			var obj=null;
+			var dfts={
+				title:'提示',
+				msg:'提示！',
+				icon:'info',
+				allowMultiple:false,
+				style:{
+					basic:'small',//basic,size
+					header:'left aligned',
+					content:'center aligned',
+					actions:'right aligned',
+					buttons:''//inverted
+				},
+				buttons:[//按钮，基于semantic ui
+					'<button class="ui green button approve"><i class="checkmark icon"></i>好</button>'
+				],
+				onApprove:function(){
+				},
+				onDeny:function(){
+				}
+			};
+			opts=$.extend(dfts,opts);
+			
+			obj=app.ui.modal(opts);
+			obj.modal('show');
+		},
+		dialog:function(obj,opts){
+			var dfts={
+				style:{//样式，基于semantic ui
+					basic:'small',//small,large,fullscreen
+					header:'left aligned',
+					content:'left aligned',
+					actions:'right aligned',
+				},
+				observeChanges:true,
+				allowMultiple:true,//是否同时打开多个
+				context:'body',//上下文
+				closeBtn:true,//是否显示关闭按钮
+				closable:false,//点击遮罩区关闭
+				transition:'scale',//呈现动画
+				blurring:false,//遮罩模糊
+				duration:animateDuration,
+				selector:{
+					approve:'.actions .approve',
+					deny:'.actions .deny',
+					close:'.actions .close,.ui-dialog-close'
+				},
+				dimmerSettings:{
+					opacity:0.6,
+					onChange:function(){
+						app.ui.draw();
+					}
+				},
+				onHidden:function(){
+					if(!opts.allowMultiple){
+						$(obj).remove();
+					}
+				},
+				onApprove:function(){
+				},
+				onDeny:function(){
+				}
+			};
+			opts=$.extend(dfts,opts);
+			
+			if(!opts.blurring){
+				$(opts.context).removeClass('blurring');
+			}
+
+			if(!opts.closeBtn){
+				obj.find('.ui-dialog-close').hide();
+			}
+
+			$(obj).addClass(opts.style.basic);
+			$(obj).children('.header').first().addClass(opts.style.header);
+			$(obj).children('.content').first().addClass(opts.style.content);
+			$(obj).children('.actions').first().addClass(opts.style.actions);
+
+			$(obj).modal(opts);
+			$(obj).modal('show');
+
+			return $(obj);
+		},
+		message:function(opts){
+			var dfts={
+				type:'info',//success,info,warning,error
+				size:'small',
+				msg:'消息',
+				closeBtn:true,
+				icon:'smile',//warning,idea,announcement,comment,info,checkmark,remove,ban,frown,meh,smile
+				html:''+
+					'<div class="ui icon hidden message">'+
+					'	<i class="close icon ui-message-close"></i>'+
+					'	<div class="content"></div>'+
+					'</div>',
+				context:elems.module
+			};
+			opts=$.extend(dfts,opts);
+			
+			var obj=$(opts.html).addClass(opts.type),
+				 closeBtnObj=obj.find('.ui-message-close');
+			
+			if(opts.closeBtn){
+				closeBtnObj.on('click',function(){
+					obj.transition('fade up',function(){
+						obj.remove();
+						app.ui.draw();
+					});
+				});
+			}else{
+				closeBtnObj.hide();
+			}
+			if(opts.icon){
+				obj.prepend('<i class="'+opts.icon+' icon"></i>');
+			}
+			obj.find('.content').html(opts.msg);
+			
+			$(opts.context).prepend(obj);
+			obj.transition('fade down',function(){
+				app.ui.draw();
+			});
+
+		},
+	};
+
+	
+	
+	//dom ready
+	$(function(){
+		app.checkRE();
+
+		app.ui.init();
+		
+		//visibility
+		$('body').visibility({
+			once:true,
+			continuous:false,
+			checkOnRefresh:true,
+			onRefresh:function(){
+				app.ui.response();
+				app.ui.draw();
+			}
+		});
+		$('body').visibility('refresh');
+
+		//logout
+		$(elems.btnLogout).off('click.logout').on('click.logout',function(){
+			app.ui.confirm({
+				title:'',
+				msg:'是否退出系统？',
+				icon:'huge help circle',
+				style:{
+					basic:'basic small',
+					header:'center aligned',
+					content:'center aligned',
+					actions:'center aligned',
+					buttons:'inverted'
+				},
+				onApprove:function(){
+					app.cookie.removeAll();
+					alert('退出！');
+				}
+			});
+		});
+
+		//first load page
+		$(elems.pageBuffer).transition('fade out',function(){
+			$(elems.pageBuffer).remove();
+			$('body').removeClass('dimmed');
+			app.ui.draw();
+		});
+		
+		//call dialogs
+		$(elems.chartBlocks).find('.ui-btn-max').off('click').on('click',function(e){
+			e.preventDefault();
+			var dialog=$('#detail'),
+				 url=$(this).attr('data-content');
+			app.ui.loadModule({
+				obj:$(dialog).children('.content'),
+				href:app.path.module+url,
+				bufferContext:dialog,
+				success:function(){
+					app.ui.dialog(dialog,{
+						style:{
+							basic:'fullscreen'
+						}
+					});
+				}
+			});
+		});
+		
+	});
+
+	
+})(jQuery,app,window);
+
+
+
